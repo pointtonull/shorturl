ce8b08c99ecbab9e86dbbaf260d9b15a1eefb4c0
Carlos Marcelo Cabrera <point_to_null@esdebian.org>
2010-08-31 03:48:06 -0300

    Added counter feature support

--- shorturl.py
+++ shorturl.py
@@ -4,26 +4,36 @@
 import urllib
 import urllib2
 import re
+import os
 import fileinput
 
 URLREGEX = r'''(?i)(?:http|ftp)s?://[]:/?#@!$&'()*+,;=A-z\d\-._~%[]*'''
 URLSERVICE = '''http://shorturl.com/make_shorturl.php?longurl=%s'''
 SHORTREGEX = r'''(?s)id="txtfld".*?value\s*=\s*"(.*?)"'''
-PREVIEWREGEX = r'''(?s)id="txtfld2".*?value\s*=\s*"(.*?)"'''
 COUNTERREGEX = r'''(?s)id="txtfld3".*?value\s*=\s*"(.*?)"'''
+LOGFILE = os.path.expanduser('''~/.shorturl''')
 
-def shrink(longurl):
+def shrink(longurl, log=True):
     queryurl = URLSERVICE % urllib.quote(longurl)
     html = "\n".join(urllib2.urlopen(queryurl).readlines())
     shorturl = re.search(SHORTREGEX, html).group(1)
+
+    if log:
+        counterurl = re.search(COUNTERREGEX, html).group(1)
+        file = open(LOGFILE, "w")
+        file.write("%s %s\n" % (longurl, counterurl))
+        file.close()
+
+
     return shorturl
+
 
 def main():
 
     for line in fileinput.input():
         for match in re.finditer(URLREGEX, line):
             line = line.replace(match.group(), shrink(match.group()))
-        print(" > %s" % line),
+        print line,
 
 
 if __name__ == "__main__":
