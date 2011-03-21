c4abe38bc915d5bf50dd4d3485ed6058c73124a9
Carlos Cabrera <point_to_null@esdebian.org>
2011-03-21 02:53:20 -0300

    Basic persistency added

--- shorturl.py
+++ shorturl.py
@@ -13,12 +13,28 @@ URLSERVICE = '''http://shorturl.com/make_shorturl.php'''
 SHORTREGEX = r'''(?s)id="txtfld".*?value\s*=\s*"(.*?)"'''
 COUNTERREGEX = r'''(?s)id="txtfld3".*?value\s*=\s*"(.*?)"'''
 LOGFILE = os.path.expanduser('''~/.shorturl''')
+FIELDSEPARATOR = r'''*FS*'''
+
+
+class Log(object):
+    def __init__(self, logfile):
+        self.logfile = logfile
+        self.entries = dict((line.strip().split(FIELDSEPARATOR)
+            for line in open(self.logfile).readlines()))
+
+    def append(self, longurl, shorturl):
+        with open(self.logfile, "a") as file:
+            file.write(FIELDSEPARATOR.join((longurl, shorturl + "\n")))
+        self.entries[shorturl] = longurl
+        return True
 
 
 @Retry(10)
 def shrink(longurl, log=True):
     if len(longurl) < 30:
         return longurl
+    elif longurl in global_log.entries:
+        return global_log.entries[longurl]
     else:
         longurl = urllib.quote_plus(longurl)
         data = urllib.urlencode({"longurl": longurl, "x": "30", "y": "7"})
@@ -30,12 +46,7 @@ def shrink(longurl, log=True):
 
         if log:
             counterurl = re.search(COUNTERREGEX, html).group(1)
-            try:
-                file = open(LOGFILE, "a")
-            except:
-                file = open(LOGFILE, "w")
-            file.write("%s %s\n" % (longurl, counterurl))
-            file.close()
+            global_log.append(longurl, counterurl)
 
         return shorturl
 
@@ -49,4 +60,5 @@ def main():
 
 
 if __name__ == "__main__":
+    global_log = Log(LOGFILE)
     exit(main())
